57320a73985b503fbf781585e879ac85e4529b2e
root <root@icr05.sincrobox.cloud>
2019-04-27 21:55:54 -0500

    last fixes

--- importrows.py
+++ importrows.py
@@ -13,6 +13,7 @@ if len(sys.argv) > 1:
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 filename = dir_path + "/" + name
+filename = '/root/data.json'
 
 with open(filename, "r") as lines:
     for line in lines:
@@ -22,6 +23,7 @@ with open(filename, "r") as lines:
         template_line = "INSERT INTO `EventData` VALUES ('{{account}}','{{device}}',{{time.epoch_time}},{{deviceData.gpsdata.event}},{{deviceData.gpsdata.latitude}},{{deviceData.gpsdata.longitude}},10,{{deviceData.gpsdata.speed}},{{deviceData.gpsdata.heading}},{{deviceData.gpsdata.altitude}},'',0,0,0,'{{deviceData.gpsdata.address}}','','',{{deviceData.distanceDelta|default(0,true)}},{{deviceData.mileage|default(0,true)}},0,0,'',{{time.epoch_time}},'','','','','','',0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'',0,'',0,'',0,0,0,'','','','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,'','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,0,0,'','','','','','') ON DUPLICATE KEY UPDATE timestamp = timestamp;"
         #template_line = "$vehicle ... $account ... $time.epoch_time"
         tm = Template(template_line)
-        item = json.load(line)
+        #print(line)
+        item = json.loads(line)
         result = tm.render(item)
         print(result)
